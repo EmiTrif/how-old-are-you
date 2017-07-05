537b5ed188cdd574dde6c57d9b61acdac13a20e4
EmiTrif <emitrif215@yahoo.com>
2017-07-05 22:14:46 +0200

    added files
    added html, js changed md

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# how-old-are-you
-a small script that asks for your age
+# How old are you?
+
+a small script that askes for your age.
+
+### Built with:
+
+	- HTML
+	- JS
+
+### Live link:
+
+	https://output.jsbin.com/lavumeq/2
---
+++ how-old.js
@@ -0,0 +1,11 @@
+
+var age = prompt('Hany eves vagy?');
+
+if (age > 18) {
+  alert("Köszönjük! Elolvashatod ezt a cikket.");
+} else if (age==18){
+  alert("Pont 18? Gratulálunk, üdv a felnőttek között! Jó szórakozást a cikkhez!");
+}
+  else {
+ alert("Sajnáljuk, de ez a cikk csak 18 éven felüliek számára érhető el.");
+}
